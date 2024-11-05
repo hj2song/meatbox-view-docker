4aa59fff62c5c443f0404170cd1ab6dacd720a61
meatbox <>
2024-11-05 18:58:30 +0900

    디렉토리명 수정

--- view/deploy/deploy.sh
+++ view/deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 
 cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/img/* /home/DATA/share/meatbox2/static/img/ && exit 1
-cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/font/* /home/DATA/share/meatbox2/static/font/ && exit 1
+cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/fonts/* /home/DATA/share/meatbox2/static/fonts/ && exit 1
 cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/css/* /home/DATA/share/meatbox2/static/css/ && exit 1
 cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/js/* /home/DATA/share/meatbox2/static/js/ && exit 1
 
