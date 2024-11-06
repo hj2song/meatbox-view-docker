82b5a2054ac8dff6be630e953de2ae4547f32011
meatbox <>
2024-11-06 10:18:20 +0900

    슬랙 알람 해제

--- view/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/ sh
-
-
-cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/img/* /home/DATA/share/meatbox2/static/img/ && exit 1
-cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/fonts/* /home/DATA/share/meatbox2/static/fonts/ && exit 1
-cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/css/* /home/DATA/share/meatbox2/static/css/ && exit 1
-cp -rp /home/DATA/www-deploy/meatbox-web2/src/main/webaap/js/* /home/DATA/share/meatbox2/static/js/ && exit 1
-
-exit 0
