c4b34a33f271a3e0b1cf811f354a3fa6c833f9da
meatbox <>
2024-11-06 10:59:00 +0900

    스크립트 변경

--- view/deploy/deploy.sh
+++ view/deploy/deploy.sh
@@ -4,20 +4,14 @@ set -ex  # 모든 명령어 실행 로그 출력 및 오류 발생 시 즉시 �
 
 echo "파일 복사를 시작합니다."
 
-# 소스 및 목적지 디렉토리 배열 정의
-declare -A dirs=(
-    ["img"]="/home/DATA/share/meatbox/static/img/"
-    ["fonts"]="/home/DATA/share/meatbox/static/fonts/"
-    ["css"]="/home/DATA/share/meatbox/static/css/"
-    ["js"]="/home/DATA/share/meatbox/static/js/"
-)
+# 복사할 디렉토리 목록 정의
+DIRS="img fonts css js"
 
-# 파일 복사 루프
-for dir in "${!dirs[@]}"; do
+for dir in $DIRS; do
     SRC="/home/DATA/www-deploy/meatbox-web2/src/main/webapp/${dir}/"
-    DEST="${dirs[$dir]}"
+    DEST="/home/DATA/share/meatbox/static/${dir}/"
     echo "복사: $SRC -> $DEST"
-    cp -afv "${SRC}"* "${DEST}" || { echo "Error: $dir 파일 복사 실패"; exit 1; }
+    cp -avf "${SRC}"* "${DEST}" || { echo "Error: $dir 파일 복사 실패"; exit 1; }
 done
 
 echo "파일 복사가 완료되었습니다."
