850c8c26b440a4df44d34bf314a30d2cafed339d
meatbox <>
2024-11-06 10:52:34 +0900

    스크립트 변경

--- view/deploy/deploy.sh
+++ view/deploy/deploy.sh
@@ -1,34 +1,24 @@
 #!/usr/bin/env sh
 
-set -ex  # 오류 발생 시 스크립트 즉시 종료 및 명령어 출력
+set -ex  # 모든 명령어 실행 로그 출력 및 오류 발생 시 즉시 종료
 
 echo "파일 복사를 시작합니다."
 
-# 소스 및 목적지 디렉토리 정의
-IMG_SRC=/home/DATA/www-deploy/meatbox-web2/src/main/webapp/img/
-IMG_DEST=/home/DATA/share/meatbox2/static/img/
+# 소스 및 목적지 디렉토리 배열 정의
+declare -A dirs=(
+    ["img"]="/home/DATA/share/meatbox/static/img/"
+    ["fonts"]="/home/DATA/share/meatbox/static/fonts/"
+    ["css"]="/home/DATA/share/meatbox/static/css/"
+    ["js"]="/home/DATA/share/meatbox/static/js/"
+)
 
-FONTS_SRC=/home/DATA/www-deploy/meatbox-web2/src/main/webapp/fonts/
-FONTS_DEST=/home/DATA/share/meatbox2/static/fonts/
-
-CSS_SRC=/home/DATA/www-deploy/meatbox-web2/src/main/webapp/css/
-CSS_DEST=/home/DATA/share/meatbox2/static/css/
-
-JS_SRC=/home/DATA/www-deploy/meatbox-web2/src/main/webapp/js/
-JS_DEST=/home/DATA/share/meatbox2/static/js/
-
-# 파일 복사
-echo "복사: $IMG_SRC -> $IMG_DEST"
-cp -rp "$IMG_SRC"* "$IMG_DEST"
-
-echo "복사: $FONTS_SRC -> $FONTS_DEST"
-cp -rp "$FONTS_SRC"* "$FONTS_DEST"
-
-echo "복사: $CSS_SRC -> $CSS_DEST"
-cp -rp "$CSS_SRC"* "$CSS_DEST"
-
-echo "복사: $JS_SRC -> $JS_DEST"
-cp -rp "$JS_SRC"* "$JS_DEST"
+# 파일 복사 루프
+for dir in "${!dirs[@]}"; do
+    SRC="/home/DATA/www-deploy/meatbox-web2/src/main/webapp/${dir}/"
+    DEST="${dirs[$dir]}"
+    echo "복사: $SRC -> $DEST"
+    cp -afv "${SRC}"* "${DEST}" || { echo "Error: $dir 파일 복사 실패"; exit 1; }
+done
 
 echo "파일 복사가 완료되었습니다."
 
